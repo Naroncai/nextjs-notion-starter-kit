314538431a0433cdf0a4f414e2d5d86fd5ca9a99
Naroncai <44219765+Naroncai@users.noreply.github.com>
2023-04-25 10:42:34 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -12,7 +12,7 @@ export const GitHubShareButton: React.FC = () => {
       aria-label='View source on GitHub'
     >
       <svg
-<!--         width='80'
+        width='80'
         height='80'
         viewBox='0 0 250 250'
         style={{
@@ -25,7 +25,7 @@ export const GitHubShareButton: React.FC = () => {
           border: 0,
           transform: 'scale(1, 1)'
         }}
-        aria-hidden='true' -->
+        aria-hidden='true'
       >
         <path d='M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z' />
         <path
